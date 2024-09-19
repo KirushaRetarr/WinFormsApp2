a32c5e4f715b4e9f3cce7844054e9b88bcc0a449
Kirill Durnin <kirill2308@mail.ru>
2024-09-19 05:50:10 +0500

    v1.2.0

--- Form1.cs
+++ Form1.cs
@@ -9,8 +9,8 @@ namespace WinFormsApp2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox2.Text = ((Math.Sin((double.Pi/2)+(3*double.Parse(textBox1.Text))))/1-(Math.Sin((3*double.Parse(textBox1.Text))-double.Pi))).ToString();
-            textBox3.Text = "OK";
+            textBox2.Text = (Math.Sin((Math.PI / 2) + 3 * double.Parse(textBox1.Text)) / 1 - Math.Sin(3 * double.Parse(textBox1.Text) - Math.PI)).ToString(); ;
+            textBox3.Text = (1 / Math.Tan(5 / 4 * Math.PI + 3 / 2 * double.Parse(textBox1.Text))).ToString(); ;
         }
 
         private void button2_Click(object sender, EventArgs e)
